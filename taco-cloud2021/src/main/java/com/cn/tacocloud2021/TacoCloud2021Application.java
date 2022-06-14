141f3079ff52d023f5ad76418d237cb2269f281a
kangyuh <kangyuh@yonyou.com>
2022-06-14 16:30:33 +0800

    代码提交

--- taco-cloud2021/src/main/java/com/cn/tacocloud2021/TacoCloud2021Application.java
+++ taco-cloud2021/src/main/java/com/cn/tacocloud2021/TacoCloud2021Application.java
@@ -2,12 +2,12 @@ package com.cn.tacocloud2021;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TacoCloud2021Application {
-
     public static void main(String[] args) {
         SpringApplication.run(TacoCloud2021Application.class, args);
     }
-
 }
