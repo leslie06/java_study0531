d7a8166d0079150b9a6e0d13dc427f14fbed0df1
kangyu <kangyu@richinfo.cn>
2023-02-13 11:12:14 +0800

    httpclient工具类

---
+++ mysqldemo/src/main/java/com/mysql/mysqldemo/entity/HttpClientResult.java
@@ -0,0 +1,63 @@
+package com.mysql.mysqldemo.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @description: 封装httpClient响应结果
+ * @author: kangy
+ * @date: 2023/2/13 10:25
+ */
+public class HttpClientResult implements Serializable {
+    private static final long serialVersionUID = 2168152194164783950L;
+
+    /**
+     * 响应状态码
+     */
+    private int code;
+
+    /**
+     * 响应数据
+     */
+    private String content;
+
+    public HttpClientResult() {
+    }
+
+    public HttpClientResult(int code) {
+        this.code = code;
+    }
+
+    public HttpClientResult(String content) {
+        this.content = content;
+    }
+
+    public HttpClientResult(int code, String content) {
+        this.code = code;
+        this.content = content;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "HttpClientResult [code=" + code + ", content=" + content + "]";
+    }
+
+
+}
